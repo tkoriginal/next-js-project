5ff933d474c52d9368f808d13226922569813307
Tauqueer Khan <tauqueer.khan@gmail.com>
2020-11-22 14:43:10 -0500

    server render initial blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
+import { GetServerSideProps } from 'next'
 import Link from 'next/link'
 
 import fire from '../config/fire-config'
 import CreatePost from '../components/CreatePost'
 
-const Home = () => {
-  const [blogs, setBlogs] = useState([])
+const Home = ({ blogs }) => {
+  const [reFetchedBlogs, setReFetchedBlogs] = useState(blogs || [])
   useEffect(() => {
     fetch('/api/getBlogs')
       .then((data) => data.json())
-      .then((blogs) => setBlogs(blogs))
-  }, [])
-  console.log(blogs)
+      .then((blogs) => setReFetchedBlogs(blogs))
+  }, [blogs])
   return (
     <div>
       <Head>
         <title>Blog App</title>
       </Head>
       <h1>Blog</h1>
-      {blogs.map((blog) => (
+      {reFetchedBlogs.map((blog) => (
         <li key={blog.id}>
           <Link href="/blog/[id]" as={'/blog/' + blog.id}>
             <a>{blog.title}</a>
@@ -29,5 +29,14 @@ const Home = () => {
       <CreatePost />
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const blogs = await fetch('http://localhost:3000/api/getBlogs')
+    .then((data) => data.json())
+    .then((blogs) => blogs)
+  return {
+    props: { blogs }
+  }
 }
 export default Home
